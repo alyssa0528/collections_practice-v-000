5aad49d03965a7d81a0218130b87c7bdb3c7c52e
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-07 20:30:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end 
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end 
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    b <=> a
+  end 
+end 
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.collect do |name|
+    name.delete(3)
+    kesha_array << name.insert(2, "$")
+  end 
+  kesha_array
+end 
+
+def find_a(array)
+  |name|
+  name.start_with?("a") 
+end 
